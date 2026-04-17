4d471c86d77fea29f9ad26fd0edf89dabe0b919e
Ranjiit Sir - FORK INFOSYSTEMS <ranjiitsir@gmail.com>
2026-04-17 15:08:04 +0530

    Add files via upload

---
+++ Dynamic Memory Allocation 17-4-2026/Student Structure/01 Accept & Display Student Details - HW.C
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////
+///
+/// Student Details Structures Accept & Display
+///
+//////////////////////////////////////////////
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct Stud
+{
+        int R_No;
+        char Name[40];
+        int Phy;
+        int Chem;
+        int Maths;
+        int Tot;
+        float Per;
+};
+
+int main()
+{
+            struct Stud Std1;
+
+            // Accept Student details
+            printf("\n\n Accepting %d Student Details => \n", StdCnt);
+
+            // Display Student details
+            printf("\n Display Student Details =>\n");
+
+       
+            printf("\n\n===============******Thanks*****=================\n");
+
+            return 0;
+}
